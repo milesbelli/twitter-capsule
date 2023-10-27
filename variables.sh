99843f892176e76ba0cd2ec994509825169105b2
Louis Mitas <lmitas@outlook.com>
2023-10-26 23:23:52 -0400

    Fixing comments
    Just some minor commenting fixes.

--- variables.sh
+++ variables.sh
@@ -1,6 +1,6 @@
 # Put the access token you get when you set up your app here (no quotes)
 export ACCESS_TOKEN=
-# Define privacy for tweets and replies independently. Options are PUBLIC, UNLISTED, PRIVATE, SKIP
+# Define privacy for tweets and replies independently. Options are public, unlisted, private, skip
 export TWEET_PRIVACY=unlisted
 export REPLY_PRIVACY=private
 # Set the offset in years that you want to go back. Must be integer greater than 0
